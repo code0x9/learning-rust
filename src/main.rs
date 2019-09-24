6c6a5b65aec04d7a008a3b3431ca0f165cbebe0d
Mark Lee <mark2@linecorp.com>
2019-09-24 13:11:43 +0900

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
-use std::io;
+extern crate rand;
 
-/// This is a doc comment! It gets documented.
-///
-/// # Examples
-///
-/// ```
-/// println!("Hello!")
-/// ```
-pub fn foo() {}
+use std::io;
+use rand::random;
+
+fn get_guess() -> u8 {
+    loop {
+        println!("Input guess:");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Could not read from stdin");
+
+        match guess.trim().parse::<u8>() {
+            Ok(v) => return v,
+            Err(e) => println!("Could not understand input: {}", e),
+        }
+    }
+}
+
+fn handle_guess(guess: u8, correct: u8) -> bool {
+    if guess < correct {
+        println!("Too low");
+        false
+    } else if guess > correct {
+        println!("Too high");
+        false
+    } else {
+        println!("You got it!");
+        true
+    }
+}
 
 fn main() {
     println!("Welcome to the guessing game!");
-    println!("Input guess:");
+    let correct = random::<u8>();
+//    println!("correct: {}", correct);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess);
-
-    println!("You guessed {}", guess)
+    loop {
+        let guess = get_guess();
+        if handle_guess(guess, correct) {
+            break;
+        }
+    }
 }
