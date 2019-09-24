cdae5275a2694b3867fb5a83572e088fabc871e0
Mark Lee <code0x9@gmail.com>
2019-09-24 20:31:45 +0900

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::random;
 
 fn get_guess() -> u8 {
     loop {
         println!("Input guess:");
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Could not read from stdin");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Could not read from stdin");
 
         match guess.trim().parse::<u8>() {
             Ok(v) => return v,
@@ -17,17 +20,22 @@ fn get_guess() -> u8 {
 }
 
 fn handle_guess(guess: u8, correct: u8) -> bool {
-    if guess < correct {
-        println!("Too low");
-        false
-    } else if guess > correct {
-        println!("Too high");
-        false
-    } else {
-        println!("You got it!");
-        true
-    }
+    return match guess.cmp(&correct) {
+        Ordering::Less => {
+            println!("Too low");
+            false
+        }
+        Ordering::Greater => {
+            println!("Too high");
+            false
+        }
+        Ordering::Equal => {
+            println!("You got it!");
+            true
+        }
+    };
 }
+
 
 fn main() {
     println!("Welcome to the guessing game!");
